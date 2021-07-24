8a78cfc32ce22132dbf89b17fae8ea04e99794bc
RadarRadarRadar <mharold214@gmail.com>
2021-07-24 12:22:08 -0500

    Create Post and Delete Api calls - Need to add try/catch to calls for error handling

--- src/services/itemService.js
+++ src/services/itemService.js
@@ -10,3 +10,26 @@ export async function getAllItems() {
       { mode: "cors" });
     return await res.json();
   }
+
+  export async function postItem(item) {
+    const res = await fetch(
+      BASE_URL,
+      {
+        method: "POST",
+        headers: { Authorization: "Bearer " + tokenService.getToken() },
+        body: JSON.stringify(item)
+      },
+      { mode: "cors" });
+    return await res.json();
+  }
+
+  export async function deleteItem(item) {
+    const res = await fetch(
+      BASE_URL,
+      {
+        method: "DELETE",
+        headers: { Authorization: "Bearer " + tokenService.getToken() },
+      },
+      { mode: "cors" });
+    return await res.json();
+  }
