822b84b3ec08e144ce64d60c8b33662397eea7b0
Matt Harold <74169347+RadarRadarRadar@users.noreply.github.com>
2021-07-25 10:27:09 -0500

    Material Ui card for item display

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,20 +1,50 @@
-import styled from 'styled-components'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-const ItemCardStyles = styled.div`
-    margin: auto;
-`
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
 
+export default function ItemCard(props) {
+  const classes = useStyles();
+  const { title, picture, description, condition } = props
 
-export default function ItemCard (props) {
-    const { title, picture, description, condition } = props
-    return (
-        <>
-            <ItemCardStyles >
-                <div className="title" >{title}</div>
-                <div className="picture" >{picture}</div>
-                <div className="description" >{description}</div>
-                <div className="condition" >{condition}</div>
-            </ItemCardStyles>
-        </>
-    )
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={picture}
+          title={title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {condition}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Wishlist
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
