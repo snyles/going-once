bcd17fa161519bfa534cf36e1d169836999490f5
snyles <sniles@gmail.com>
2021-07-26 19:16:16 -0400

    add delete item functionality

--- src/pages/Item.js
+++ src/pages/Item.js
@@ -1,17 +1,21 @@
-import { useEffect, useState } from "react";
-import { Link, Redirect, useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Link, Redirect, useHistory, useParams } from "react-router-dom";
 import Page from "../components/Page";
 import { ColOne, ColTwo, TwoCols } from "../components/Styles/TwoCols";
 import * as itemService from '../services/itemService'
 import ItemMap from '../components/ItemMap'
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import styled from 'styled-components'
+import { UserContext } from "../lib/UserContext";
+import { Button } from "@material-ui/core";
 
 const PageNav = styled.div`
   text-align: left;
   font-size: 2rem;
 `;
 export default function ItemPage() {
+  const user = useContext(UserContext)
+  const history = useHistory()
   const {id} = useParams();
   const [itemData, setItemData] = useState(null)
   const [error, setError] = useState(null)
@@ -23,6 +27,12 @@ export default function ItemPage() {
     }
     if (id) fetchItem(id)
   }, [id])
+
+  const deleteItem = async () => {
+    if (itemData?.owner !== user._id) return
+    const result = await itemService.deleteItem(itemData?._id)
+    history.push('/items')
+  }
 
   if (!id) return <Redirect to="/items" />
   return (
@@ -41,6 +51,15 @@ export default function ItemPage() {
           <p>{itemData?.category}</p>
           <p>{itemData?.lat}</p>
           <p>{itemData?.lng}</p>
+          {user._id === itemData?.owner && 
+            <Button 
+              variant="contained" 
+              color="secondary"
+              onClick={deleteItem}
+            >
+              Delete Item
+            </Button>
+          }
         </ColOne>
         <ColTwo>
           {itemData?.lat && itemData?.lng && 
